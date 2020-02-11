3710a3d5225b6216c53a701d2e6fb0634b243754
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-11 18:58:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(s)
+
+  number = 0
+  while s < 10
+  
+  puts " -#{number}SECOND#{s}!"
+  s +=1
 end
+  s = 10
+  until s == 11 
+    return "HAPPY NEW YEAR!"
+    s +=1
+
+end
+end
+
+countdown(1)
