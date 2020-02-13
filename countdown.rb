291129de44715c7ed00926fea8a0d92aa4abd05f
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-13 22:26:48 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
-
-def countdown
-end
+def countdown(x)
+  x = 10
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    def countdown_with_sleep(x)
+    sleep(x)
+  end
+      x -= 1
+  end
+return "HAPPY NEW YEAR!"
+  end
