43cd1e557b44bad857737e50064cff86c6c66741
Dural Keys <duralinitaly@gmail.com>
2020-02-18 04:04:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(num)
+   while num > 0
+   puts num.to_s + ' SECOND(S)!'
+   num -= 1
+   end
+  'HAPPY NEW YEAR!'
+
 end
+
+
+
+def countdown_with_sleep(num)
+  
+  while num > 0
+   puts num.to_s + ' SECOND(S)!'
+   num -= 1
+  'HAPPY NEW YEAR!'
+  sleep(1)
+  end
+  'HAPPY NEW YEAR!'
+end 
